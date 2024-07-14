87ef1fe5c457d04b55bbbe665663342e0d324dbb
Rajeshbabu21 <rajeshgit2004@gmail.com>
2024-07-14 12:50:57 +0530

    deleted some code

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,36 +1,3 @@
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    pricecents: 1090,
-  },
-
-  // second product
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4.0,
-      count: 127,
-    },
-    pricecents: 2095,
-  },
-
-  // third products
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    pricecents: 799,
-  },
-];
 
 // to combine the html to webpage
 let productHTML = ''
@@ -60,7 +27,7 @@ products.forEach((product) => {
 
       
       <div class="product-price">
-        ${(product.pricecents / 100).toFixed(2)}  
+        ${(product.priceCents / 100).toFixed(2)}  
       </div>
 
       <div class="product-quantity-container">
