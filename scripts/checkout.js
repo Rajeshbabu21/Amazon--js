b11f4fae5f740c2fc1ef10e686b09b7c1b80d725
Rajeshbabu21 <rajeshgit2004@gmail.com>
2024-07-15 22:58:47 +0530

    added the delete button

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,113 +1,113 @@
 // save the data
-import {
-  cart 
-} from '../data/cart.js';
+import { cart ,removecart} from "../data/cart.js";
 
-import {
-  products
-} from '../data/products.js'
-
+import { products } from "../data/products.js";
 
 let cartsummary = "";
 // to come out the id and search
-cart.forEach((cartitem) =>{
+cart.forEach((cartitem) => {
   const productId = cartitem.productId;
   let matchinproduct;
 
-  // to match the id 
-  products.forEach((product) =>{
-    if(product.id === productId) 
-    {
+  // to match the id
+  products.forEach((product) => {
+    if (product.id === productId) {
       matchinproduct = product;
     }
-
   });
   // console.log(matchinproduct);
 
-
-
-
- cartsummary += `
+  cartsummary += `
   <div class="cart-item-container">
-            <div class="delivery-date">
-              Delivery date: Tuesday, June 21
-            </div>
+  <div class="delivery-date">
+    Delivery date: Tuesday, June 21
+  </div>
 
-            <div class="cart-item-details-grid">
-              <img class="product-image"
-                src="${matchinproduct.image}">
+  <div class="cart-item-details-grid">
+    <img class="product-image"
+      src="${matchinproduct.image}">
 
-              <div class="cart-item-details">
-                <div class="product-name">
-                  ${matchinproduct.name}
-                </div>
-                <div class="product-price">
-                  ${(matchinproduct.priceCents / 100).toFixed(2)}
-                </div>
-                <div class="product-quantity">
-                  <span>
-                    Quantity: <span class="quantity-label">${
-                      cart.quatity
-                    }</span>
-                  </span>
-                  <span class="update-quantity-link link-primary">
-                    Update
-                  </span>
-                  <span class="delete-quantity-link link-primary">
-                    Delete
-                  </span>
-                </div>
-              </div>
+    <div class="cart-item-details">
+      <div class="product-name">
+        ${matchinproduct.name}
+      </div>
+      <div class="product-price">
+        ${(matchinproduct.priceCents / 100).toFixed(2)}
+      </div>
+      <div class="product-quantity">
+        <span>
+          Quantity: <span class="quantity-label">${
+            cart.quatity
+          }</span>
+        </span>
+        <span class="update-quantity-link link-primary">
+          Update
+        </span>
+        <span class="delete-quantity-link link-primary js-delete-link" data-product-id=${matchinproduct.id}  >
+          Delete
+        </span>
+      </div>
+    </div>
 
-              <div class="delivery-options">
-                <div class="delivery-options-title">
-                  Choose a delivery option:
-                </div>
-                <div class="delivery-option">
-                  <input type="radio" checked
-                    class="delivery-option-input"
-                    name="delivery-option-${matchinproduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Tuesday, June 21
-                    </div>
-                    <div class="delivery-option-price">
-                      FREE Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchinproduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Wednesday, June 15
-                    </div>
-                    <div class="delivery-option-price">
-                      $4.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchinproduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Monday, June 13
-                    </div>
-                    <div class="delivery-option-price">
-                      $9.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+    <div class="delivery-options">
+      <div class="delivery-options-title">
+        Choose a delivery option:
+      </div>
+      <div class="delivery-option">
+        <input type="radio" checked
+          class="delivery-option-input"
+          name="delivery-option-${matchinproduct.id}">
+        <div>
+          <div class="delivery-option-date">
+            Tuesday, June 21
           </div>
+          <div class="delivery-option-price">
+            FREE Shipping
+          </div>
+        </div>
+      </div>
+      <div class="delivery-option">
+        <input type="radio"
+          class="delivery-option-input"
+          name="delivery-option-${matchinproduct.id}">
+        <div>
+          <div class="delivery-option-date">
+            Wednesday, June 15
+          </div>
+          <div class="delivery-option-price">
+            $4.99 - Shipping
+          </div>
+        </div>
+      </div>
+      <div class="delivery-option">
+        <input type="radio"
+          class="delivery-option-input"
+          name="delivery-option-${matchinproduct.id}">
+        <div>
+          <div class="delivery-option-date">
+            Monday, June 13
+          </div>
+          <div class="delivery-option-price">
+            $9.99 - Shipping
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
   `;
+});
 
+document.querySelector(".order-summary").innerHTML = cartsummary;
 
-})
+// to delete the item to all the button
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
 
-document.querySelector(".order-summary").innerHTML= cartsummary;
+    // console.log(productId);
+    // to remove the id
+    removecart(productId)
+    console.log(cart)
+  });
+});
