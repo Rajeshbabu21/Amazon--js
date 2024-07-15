4bf1abf75c530cccafdfcc24049587e987933803
Rajeshbabu21 <rajeshgit2004@gmail.com>
2024-07-15 19:03:12 +0530

    added the module function and group the code

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,26 @@
 export const cart = [];
+
+export function addtocart(productId) {
+  let matchingitem;
+
+  // to check the the product is exits or not
+  cart.forEach((cartitem) => {
+    if (cartitem.productId === productId) {
+      matchingitem = cartitem;
+    }
+  });
+
+  // y means if item found it always incremenet
+  if (matchingitem) {
+    matchingitem.quantity += 1;
+  }
+
+  // if not set to i
+  else {
+    //push to the cart
+    cart.push({
+      productId: productId,
+      quantity: 1,
+    });
+  }
+}
